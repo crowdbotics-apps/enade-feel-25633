02a360fefe3c1c06b72dc1391d388cd294cb900c
Crowdbotics <team@crowdbotics.com>
2021-04-12 02:13:53 +0000

    Initial commit.

---
+++ backend/enade_feel_25633/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for enade_feel_25633 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'enade_feel_25633.settings')
+
+application = get_wsgi_application()
